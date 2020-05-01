3119f20b9052ac8ebbe6dc46b1cb93204b7bc12a
suraj <surajido111@gmail.com>
2020-05-01 09:32:23 +0545

    Done

--- suraj.py
+++ suraj.py
@@ -1,2 +1,19 @@
-print("Binary Search Algorithm")
-print("suraj ale magar")
+array=list(map(int,input().split()))
+array.sort()
+print(array)
+n=int(input("enter the number you want to find its position in the list:"))
+low=0
+mid=0
+high=int(len(array))-1
+if n in array:
+    while n!=array[mid]:
+        mid=int((low+high)/2)
+        if n==array[mid]:
+            print("position is:",mid)
+        elif n>array[mid]:
+            low=mid+1
+        else:
+            high=mid-1
+else:
+    print("Entered Number is not in the list:")
+    
